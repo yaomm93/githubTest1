27c61e5233bac258495009d5d9c11f3393647279
yaomm <yaomm93@163.com>
2019-09-12 11:52:15 +0800

    已提交到本地库

---
+++ src/main/java/com/powerlbs/User.java
@@ -0,0 +1,12 @@
+package com.powerlbs;
+
+/**
+ * ClassName:${name}
+ * package:com.powerlbs
+ *
+ * @date:2019/9/12 11:36
+ * @author:Yaoxiaoming
+ **/
+public class User {
+    private String id;
+}
